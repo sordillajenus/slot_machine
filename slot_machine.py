fdce34189707c3455f9e96b34973d5786db5eac6
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-04 13:03:04 +0800

    i completed the first two block of code which the program would ask for the amount of money to bet on and the maximum number of lines that can be used. I also added a def main function to define the primary function that orchestrates a program's execution. i also added a global connstant in snake case format so that i can just call it in my program and can easily be changed whenever i want

--- slot_machine.py
+++ slot_machine.py
@@ -1,3 +1,5 @@
+max_lines = 3
+
 def deposit():
     while True:
         amount = input("Please Enter the amount you want to deposit:")
@@ -6,10 +8,28 @@ def deposit():
             if amount > 0:
                 break
             else:
-                print("Please enter a valid numerical amount:")
+                print("Please enter a valid numerical amount")
         else:
             print("Would you please enter a number?")        
 
     return amount     
 
-deposit()      
+def get_number_of_lines():
+    while True:
+        lines = input("Please enter a valid number of lines to bet on (1- " + str(max_lines) + ")?")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= max_lines:
+                break
+            else:
+                print("Please enter a valid number of lines")
+        else:
+            print("Please Enter a digit")       
+
+
+
+def main():
+    balance = deposit()
+    lines =   get_number_of_lines
+    print = (balance, lines)    
+    
