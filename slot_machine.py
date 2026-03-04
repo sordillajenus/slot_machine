6a48834be6158b52658d8f259b492ecb43901ab6
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-04 11:59:52 +0800

    setting up a define function

--- slot_machine.py
+++ slot_machine.py
@@ -1,1 +1,7 @@
-print("hello world")
+def deposit():
+    while True:
+        amount = input("Please Enter the amount you want to deposit:")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
