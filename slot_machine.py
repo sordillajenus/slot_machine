92cca9b80527882686040853ecf97f792f66449c
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-04 22:11:44 +0800

    fixed the typos and added some variables

--- slot_machine.py
+++ slot_machine.py
@@ -47,5 +47,8 @@ def main():
     balance = deposit()
     lines =   get_number_of_lines
     bet = get_amount_of_bet
-    print = (balance, lines)    
+    total_bet = bet * lines
+    print(f"You are betting ₱{bet} on {lines} lines. Total bet is: {total_bet}")
+    
+       
     
