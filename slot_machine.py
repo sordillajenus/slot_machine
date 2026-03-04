c6b15d2894ad3b2d3d43800e9463fa4a59b10591
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-04 23:37:42 +0800

    fixed the error

--- slot_machine.py
+++ slot_machine.py
@@ -4,58 +4,58 @@ MIN_BET = 1
 
 def deposit():
     while True:
-        amount = input("Please Enter the amount you want to deposit:")
+        amount = input("Please Enter the amount you want to deposit: ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
-                break
+                return amount
             else:
-                print("Please enter a valid numerical amount")
+                print("Please enter a valid numerical amount.")
         else:
             print("Would you please enter a number?")        
 
-    return amount     
 
 def get_number_of_lines():
     while True:
-        lines = input("Please enter a valid number of lines to bet on (1- " + str(MAX_LINES) + ")?")
+        lines = input(f"Please enter a valid number of lines to bet on (1-{MAX_LINES}): ")
         if lines.isdigit():
             lines = int(lines)
-            if 1<= lines <= MAX_LINES:
-                break
+            if 1 <= lines <= MAX_LINES:
+                return lines
             else:
-                print("Please enter a valid number of lines")
+                print("Please enter a valid number of lines.")
         else:
-            print("Please Enter a digit")   
+            print("Please enter a digit.")   
 
-    return lines            
 
 def get_amount_of_bet():
     while True:
-        bet = input("Please Enter a valid amount of bet on each line ₱:")
+        bet = input("Please Enter a valid amount of bet on each line ₱: ")
         if bet.isdigit():
             bet = int(bet)
-            if MAX_BET >= bet >= MIN_BET:
-                break
+            if MIN_BET <= bet <= MAX_BET:
+                return bet   
             else:
                 print(f"Amount must be between ₱{MIN_BET} - ₱{MAX_BET}")
-
-
+        else:
+            print("Please enter a number.")
 
 
 def main():
     balance = deposit()
-    lines =   get_number_of_lines()
+    lines = get_number_of_lines()
+
     while True:
         bet = get_amount_of_bet()
         total_bet = bet * lines
 
         if total_bet > balance:
-            print(f"You do not have an enough balance. Your current balance is: ₱{balance}")
+            print(f"You do not have enough balance. Your current balance is: ₱{balance}")
         else:
             break
 
-    
-    print(f"You are betting ₱{bet} on {lines} lines. Total bet is: {total_bet}")
-     
-    
+    print(f"You are betting ₱{bet} on {lines} lines.")
+    print(f"Total bet is: ₱{total_bet}")
+
+
+main()
