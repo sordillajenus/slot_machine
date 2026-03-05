0503b29009f04ff169d30617bbcca342f4e90c6f
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-05 11:31:03 +0800

    print slot machine function

--- slot_machine.py
+++ slot_machine.py
@@ -20,7 +20,7 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [[], [], []]
+    columns = []
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
@@ -32,6 +32,15 @@ def get_slot_machine_spin(rows, cols, symbols):
         columns.append(column)
 
     return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
 
 def deposit():
     while True:
